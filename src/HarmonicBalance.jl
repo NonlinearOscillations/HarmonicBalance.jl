7b74f320c01192d65aae028f03ebcfbcb37a0095
jdelpino <jdelpino>
2022-01-03 08:48:44 -0300

    split static and interactive plotting

--- src/HarmonicBalance.jl
+++ src/HarmonicBalance.jl
@@ -42,7 +42,8 @@ module HarmonicBalance
     include("sorting.jl")
     include("classification.jl")
     include("saving.jl")
-    include("plotting.jl")
+    include("plotting_static.jl")
+    include("plotting_interactive.jl")
 
     include("modules/HC_wrapper.jl")
     using .HC_wrapper

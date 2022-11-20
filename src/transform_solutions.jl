c4ace6cee0d21d7ec1f072e01126dfb746e46141
jkosata <j.kosata@protonmail.com>
2022-11-20 18:13:32 +0100

    branches kw for transform_solutions

--- src/transform_solutions.jl
+++ src/transform_solutions.jl
@@ -11,9 +11,9 @@ Takes a `Result` object and a string `f` representing a Symbolics.jl expression.
 Returns an array with the values of `f` evaluated for the respective solutions.
 Additional substitution rules can be specified in `rules` in the format `("a" => val)` or `(a => val)`
 """
-function transform_solutions(res::Result, f::String; rules=Dict())
+function transform_solutions(res::Result, f::String, branches = 1:branch_count(res); rules=Dict())
     # a string is used as input - a macro would not "see" the user's namespace while the user's namespace does not "see" the variables
-    transformed = [Vector{ComplexF64}(undef, length(res.solutions[1])) for k in res.solutions] # preallocate
+    transformed = [Vector{ComplexF64}(undef, length(branches)) for k in res.solutions] # preallocate
 
     # define variables in rules in this namespace
     new_keys = declare_variable.(string.(keys(Dict(rules))))
@@ -36,9 +36,9 @@ function transform_solutions(res::Result, f::String; rules=Dict())
     for idx in CartesianIndices(res.solutions)
         params_values = res.swept_parameters[Tuple(idx)...]
         vals[end-n_pars+1:end] .= params_values # param values are common to all branches
-        for (branch,soln) in enumerate(res.solutions[idx])
-            vals[1:n_vars] .= soln
-            transformed[idx][branch] = Base.invokelatest(f, vals)
+        for (k, branch) in enumerate(branches)
+            vals[1:n_vars] .= res.solutions[idx][branch]
+            transformed[idx][k] = Base.invokelatest(f, vals)
         end
     end
     return transformed
@@ -47,7 +47,7 @@ end
 transform_solutions(res::Result, fs::Vector{String}; kwargs...) = [transform_solutions(res, f; kwargs...) for f in fs]
 
 # a simplified version meant to work with arrays of solutions
-# cannot parse parameter values  mainly meant for time-dependent results
+# cannot parse parameter values -- meant for time-dependent results
 function transform_solutions(soln::Vector, f::String, harm_eq::HarmonicEquation)
 
     vars = _remove_brackets(get_variables(harm_eq))

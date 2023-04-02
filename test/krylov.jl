dbd085a7d4ea67fa209fb2ad424295cc85ccb8a8
Orjan Ameye <orjan.ameye@hotmail.com>
2023-04-02 23:37:05 +0200

    oeoeooe it also works for a paramtron

--- test/krylov.jl
+++ test/krylov.jl
@@ -1,13 +1,12 @@
 using HarmonicBalance
 
 @variables t T x(t) y(t) # symbolic variables
-@variables ω ω0 γ F
+@variables ω ω0 γ F α λ ψ θ η
 
-eq = [d(d(x, t),t) + ω0^2*x ~ - γ*d(x,t)]
+eq = [d(d(x,t),t) + γ*d(x,t) + ω0^2*(1-λ*cos(2*ω*t+ψ))*x + α*x^3 + η*d(x,t)*x^2 ~ F*cos(ω*t+θ)]
 
-diff_eom = DifferentialEquation(eq, [x, y])
+diff_eom = DifferentialEquation(eq, [x])
 
 add_harmonic!(diff_eom, x, ω) # x will rotate at ω
-
 
 get_krylov_equations(diff_eom)

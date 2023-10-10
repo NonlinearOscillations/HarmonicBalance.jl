9a18114b73b111950fd6e78c6b5d98c439b5cfba
Orjan Ameye <orjan.ameye@hotmail.com>
2023-10-10 16:07:49 +0200

    add css file to configure theme style

--- docs/make.jl
+++ docs/make.jl
@@ -5,9 +5,13 @@ using HarmonicBalance
 
 makedocs(
 	sitename="HarmonicBalance.jl",
+    # modules = [HarmonicBalance],
+    # warnonly = true,
 	format = Documenter.HTML(
 		mathengine=MathJax(),
-		assets = ["assets/favicon.ico"]
+        # canonical="https://nonlinearoscillations.github.io/HarmonicBalance.jl/stable/",
+		assets = ["assets/favicon.ico", "assets/docs.css"]
+        # size_threshold = nothing
 	),
 	pages = [
 		"Background" => Any[
@@ -19,7 +23,7 @@ makedocs(
 			"examples/linear_response.md"
 			"examples/time_dependent.md"
 			"examples/parametron.md"
-			],
+		],
 		"Manual" => Any[
 			"manual/entering_eom.md"
 			"manual/extracting_harmonics.md"
@@ -28,8 +32,8 @@ makedocs(
 			"manual/time_dependent.md"
 			"manual/linear_response.md"
 			"manual/saving.md"
-			]
 		]
+	]
 )
 
 deploydocs(

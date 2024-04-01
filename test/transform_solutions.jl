3e8cb300ad44f68ff5ff27ce7396418e8f351897
oameye <orjan.ameye@hotmail.com>
2024-04-01 16:51:59 +0200

    fix transform bug

--- test/transform_solutions.jl
+++ test/transform_solutions.jl
@@ -1,5 +1,9 @@
 using HarmonicBalance
 
+using Random
+const SEED = 0xd8e5d8df
+Random.seed!(SEED)
+
 @variables Ω γ λ F x θ η α ω0 ω t T ψ
 @variables x(t)
 
@@ -15,6 +19,9 @@ varied = ω => range(0.9, 1.1, 10)
 res = get_steady_states(harmonic_eq, varied, fixed, show_progress=false, seed=SEED);
 
 transform_solutions(res, "u1^2+v1^2")
+transform_solutions(res, "√(u1^2+v1^2)"; realify=true)
+
+# transform_solutions(res.solutions[1][1], "√(u1^2+v1^2)", harmonic_eq)
 
 times = 0:1:10
 soln = res[5][1]

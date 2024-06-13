87c18fbe512025255c2cd95a665d5caa49babf69
oameye <orjan.ameye@hotmail.com>
2024-06-13 08:57:29 +0200

    explicit import MTKExt and SSDExt

--- ext/ModelingToolkitExt.jl
+++ ext/ModelingToolkitExt.jl
@@ -1,16 +1,20 @@
 module ModelingToolkitExt
 
-export ODESystem, ODEProblem
+export ODESystem, ODEProblem, SteadyStateProblem, NonlinearProblem
 
 using HarmonicBalance:
-    HarmonicEquation,
-    is_rearranged,
-    rearrange_standard,
-    get_variables,
-    simplify,
-    ParameterList
-using ModelingToolkit
-import ModelingToolkit: ODESystem, ODEProblem, NonlinearProblem, SteadyStateProblem
+    HarmonicEquation, is_rearranged, rearrange_standard, get_variables, ParameterList
+using Symbolics: simplify, Equation, substitute, Num, @variables, expand
+using ModelingToolkit:
+    ModelingToolkit,
+    ODESystem,
+    ODEProblem,
+    NonlinearProblem,
+    SteadyStateProblem,
+    varmap_to_vars,
+    parameters,
+    @parameters,
+    @mtkbuild
 
 swapsides(eq::Equation) = Equation(eq.rhs, eq.lhs)
 
@@ -21,7 +25,7 @@ function declare_parameter(var::Num)
     return eval(var_sym)
 end
 
-function ODESystem(eom::HarmonicEquation)
+function ModelingToolkit.ODESystem(eom::HarmonicEquation)
     if !is_rearranged(eom) # check if time-derivatives of the variable are on the right hand side
         eom = rearrange_standard(eom)
     end
@@ -40,11 +44,11 @@ function ODESystem(eom::HarmonicEquation)
     return sys
 end
 
-function ODEProblem(
+function ModelingToolkit.ODEProblem(
     eom::HarmonicEquation, u0, tspan::Tuple, p::ParameterList; in_place=true, kwargs...
 )
     sys = ODESystem(eom)
-    param = ModelingToolkit.varmap_to_vars(p, parameters(sys))
+    param = varmap_to_vars(p, parameters(sys))
     if !in_place # out-of-place
         prob = ODEProblem{false}(sys, u0, tspan, param; jac=true, kwargs...)
     else # in-place
@@ -53,18 +57,18 @@ function ODEProblem(
     return prob
 end
 
-function NonlinearProblem(
+function ModelingToolkit.NonlinearProblem(
     eom::HarmonicEquation, u0, p::ParameterList; in_place=true, kwargs...
 )
     ss_prob = SteadyStateProblem(eom, u0, p::ParameterList; in_place=in_place, kwargs...)
     return NonlinearProblem(ss_prob) # gives warning of some internal deprication
 end
 
-function SteadyStateProblem(
+function ModelingToolkit.SteadyStateProblem(
     eom::HarmonicEquation, u0, p::ParameterList; in_place=true, kwargs...
 )
     sys = ODESystem(eom)
-    param = ModelingToolkit.varmap_to_vars(p, parameters(sys))
+    param = varmap_to_vars(p, parameters(sys))
     if !in_place # out-of-place
         prob = SteadyStateProblem{false}(sys, u0, param; jac=true, kwargs...)
     else # in-place

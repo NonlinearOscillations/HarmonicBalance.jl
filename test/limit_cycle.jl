b0698b577bbd173eab7742d7926ac562ae81cf2d
oameye <orjan.ameye@hotmail.com>
2024-06-14 11:18:21 +0200

    format

--- test/limit_cycle.jl
+++ test/limit_cycle.jl
@@ -13,7 +13,7 @@ import HarmonicBalance.LinearResponse.plot_linear_response
     harmonic_eq = get_harmonic_equations(dEOM)
     HarmonicBalance.LimitCycles._choose_fixed(harmonic_eq, ω_lc)
 
-    fixed = ();
+    fixed = ()
     varied = μ => range(2, 3, 2)
 
     result = get_limit_cycles(
@@ -23,33 +23,33 @@ import HarmonicBalance.LinearResponse.plot_linear_response
     @test sum(any.(classify_branch(result, "stable"))) == 4
     @test sum(any.(classify_branch(result, "unique_cycle"))) == 1
 
-    plot(result, y="ω_lc")
-    plot_linear_response(result, x, branch=1, Ω_range=range(2.4, 2.6, 2), order=1)
+    plot(result; y="ω_lc")
+    plot_linear_response(result, x; branch=1, Ω_range=range(2.4, 2.6, 2), order=1)
 end
 
 # takes to long
 # @testset "coupled modes" begin
-    # @variables F, ω, ω_lc, t, x(t), y(t)
+# @variables F, ω, ω_lc, t, x(t), y(t)
 
-    # eqs = [d(x,t,2) + 1.0^2*x - x^3 - 0.006*y ~ F*cos(ω*t),
-    #     d(y,t,2) + 1.005^2*y - y^3 - 0.006*x ~ 0]
+# eqs = [d(x,t,2) + 1.0^2*x - x^3 - 0.006*y ~ F*cos(ω*t),
+#     d(y,t,2) + 1.005^2*y - y^3 - 0.006*x ~ 0]
 
-    # # differential equations
-    # diffeq = DifferentialEquation(eqs, [x,y])
+# # differential equations
+# diffeq = DifferentialEquation(eqs, [x,y])
 
-    # # specify the harmonic ansatz for x and y: x = u(T) cos(ωt) + v(T) sin(ωt)
-    # add_harmonic!(diffeq, x, ω)
-    # add_harmonic!(diffeq, y, ω)
-    # add_harmonic!(diffeq, x, ω + ω_lc)
-    # add_harmonic!(diffeq, y, ω + ω_lc)
-    # add_harmonic!(diffeq, x, ω - ω_lc)
-    # add_harmonic!(diffeq, y, ω - ω_lc)
+# # specify the harmonic ansatz for x and y: x = u(T) cos(ωt) + v(T) sin(ωt)
+# add_harmonic!(diffeq, x, ω)
+# add_harmonic!(diffeq, y, ω)
+# add_harmonic!(diffeq, x, ω + ω_lc)
+# add_harmonic!(diffeq, y, ω + ω_lc)
+# add_harmonic!(diffeq, x, ω - ω_lc)
+# add_harmonic!(diffeq, y, ω - ω_lc)
 
-    # harmonic_eq = get_harmonic_equations(diffeq);
+# harmonic_eq = get_harmonic_equations(diffeq);
 
-    # fixed = (F => 0.0015)
-    # varied = (ω => range(0.992, 0.995, 2))
+# fixed = (F => 0.0015)
+# varied = (ω => range(0.992, 0.995, 2))
 
-    # # results
-    # result = get_limit_cycles(harmonic_eq, varied, fixed, ω_lc; threading=true)
+# # results
+# result = get_limit_cycles(harmonic_eq, varied, fixed, ω_lc; threading=true)
 # end

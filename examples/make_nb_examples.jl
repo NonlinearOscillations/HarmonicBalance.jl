33d78c7f35a4745ca425b4b8a4aceda273a5bcc5
Orjan Ameye <orjan.ameye@hotmail.com>
2024-09-16 09:27:20 +0200

    remove gitmodule examples (#239)

---
+++ examples/make_nb_examples.jl
@@ -0,0 +1,16 @@
+using Literate
+
+using HarmonicBalance, Plots
+default(; fmt=:png)
+
+### Process examples
+# Always rerun examples
+const EXAMPLES_IN = @__DIR__
+const OUTPUT_NB_DIR = @__DIR__
+
+examples = filter!(file -> file[(end - 2):end] == ".jl", readdir(EXAMPLES_IN; join=true))
+filter!(file -> !contains(file, "make_nb_examples"), examples)
+
+for example in examples
+    Literate.notebook(example, OUTPUT_NB_DIR; documenter=false, execute=true)
+end

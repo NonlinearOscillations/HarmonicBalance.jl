cf298bf3328cdd1cf5c6c00933ff898234a4cba4
Orjan Ameye <orjan.ameye@hotmail.com>
2024-10-13 21:51:31 +0200

    enable "Code quality" tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,41 +5,41 @@ using Random
 const SEED = 0xd8e5d8df
 Random.seed!(SEED)
 
-# @testset "Code quality" begin
-#     using ExplicitImports, Aqua
-#     using ModelingToolkit, OrdinaryDiffEqTsit5, SteadyStateDiffEq
-#     ignore_deps = [:Random, :LinearAlgebra, :Printf, :Test, :Pkg]
-#     TimeEvolution = Base.get_extension(HarmonicBalance, :TimeEvolution)
-#     ModelingToolkitExt = Base.get_extension(HarmonicBalance, :ModelingToolkitExt)
-#     SteadyStateDiffEqExt = Base.get_extension(HarmonicBalance, :SteadyStateDiffEqExt)
+@testset "Code quality" begin
+    using ExplicitImports, Aqua
+    using ModelingToolkit, OrdinaryDiffEqTsit5, SteadyStateDiffEq
+    ignore_deps = [:Random, :LinearAlgebra, :Printf, :Test, :Pkg]
+    TimeEvolution = Base.get_extension(HarmonicBalance, :TimeEvolution)
+    ModelingToolkitExt = Base.get_extension(HarmonicBalance, :ModelingToolkitExt)
+    SteadyStateDiffEqExt = Base.get_extension(HarmonicBalance, :SteadyStateDiffEqExt)
 
-#     @test check_no_stale_explicit_imports(HarmonicBalance) == nothing
-#     @test check_all_explicit_imports_via_owners(HarmonicBalance) == nothing
-#     Aqua.test_ambiguities(HarmonicBalance)
-#     Aqua.test_all(
-#         HarmonicBalance;
-#         deps_compat=(
-#             ignore=ignore_deps,
-#             check_extras=(ignore=ignore_deps,),
-#             check_weakdeps=(ignore=ignore_deps,),
-#         ),
-#         ambiguities=false,
-#     )
-#     for mod in [HarmonicBalance, TimeEvolution, ModelingToolkitExt, SteadyStateDiffEqExt]
-#         @test check_no_stale_explicit_imports(mod) == nothing
-#         @test check_all_explicit_imports_via_owners(mod) == nothing
-#         Aqua.test_ambiguities(mod)
-#         Aqua.test_all(
-#             mod;
-#             deps_compat=false,
-#             ambiguities=false,
-#             piracies=false,
-#             stale_deps=false,
-#             project_extras=false,
-#             persistent_tasks=false
-#         )
-#     end
-# end
+    @test check_no_stale_explicit_imports(HarmonicBalance) == nothing
+    @test check_all_explicit_imports_via_owners(HarmonicBalance) == nothing
+    Aqua.test_ambiguities(HarmonicBalance)
+    Aqua.test_all(
+        HarmonicBalance;
+        deps_compat=(
+            ignore=ignore_deps,
+            check_extras=(ignore=ignore_deps,),
+            check_weakdeps=(ignore=ignore_deps,),
+        ),
+        ambiguities=false,
+    )
+    for mod in [HarmonicBalance, TimeEvolution, ModelingToolkitExt, SteadyStateDiffEqExt]
+        @test check_no_stale_explicit_imports(mod) == nothing
+        @test check_all_explicit_imports_via_owners(mod) == nothing
+        Aqua.test_ambiguities(mod)
+        Aqua.test_all(
+            mod;
+            deps_compat=false,
+            ambiguities=false,
+            piracies=false,
+            stale_deps=false,
+            project_extras=false,
+            persistent_tasks=false
+        )
+    end
+end
 
 @testset "Code linting" begin
     using JET

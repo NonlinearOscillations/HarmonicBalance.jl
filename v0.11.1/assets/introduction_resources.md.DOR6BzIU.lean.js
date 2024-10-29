14db63f55e3f780bffcec8be15dd9e53b49f8c3b
Documenter.jl <documenter@juliadocs.github.io>
2024-10-29 10:57:43 +0000

    build based on 9ef6995

---
+++ v0.11.1/assets/introduction_resources.md.DOR6BzIU.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r}from"./chunks/framework.CfM1St1B.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"introduction/resources.md","filePath":"introduction/resources.md"}'),o={name:"introduction/resources.md"};function s(c,n,a,i,d,p){return r(),t("div")}const m=e(o,[["render",s]]);export{_ as __pageData,m as default};

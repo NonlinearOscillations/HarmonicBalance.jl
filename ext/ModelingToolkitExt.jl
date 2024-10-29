25e0eba12c53d5bd6f4bf9f9bd60249083544bab
Orjan Ameye <orjan.ameye@hotmail.com>
2024-10-29 10:58:33 +0100

    feat: add `ODEProblem(::DIfferentialEquation)

--- ext/ModelingToolkitExt.jl
+++ ext/ModelingToolkitExt.jl
@@ -3,8 +3,17 @@ module ModelingToolkitExt
 export ODESystem, ODEProblem, SteadyStateProblem, NonlinearProblem
 
 using HarmonicBalance:
-    HarmonicEquation, is_rearranged, rearrange_standard, get_variables, ParameterList
-using Symbolics: simplify, Equation, substitute, Num, @variables, expand, unwrap, arguments
+    HarmonicEquation,
+    is_rearranged,
+    rearrange_standard,
+    get_variables,
+    ParameterList,
+    DifferentialEquation,
+    get_independent_variables
+using HarmonicBalance.KrylovBogoliubov:
+    rearrange_standard!, is_rearranged_standard, first_order_transform!
+using Symbolics:
+    simplify, Equation, substitute, Num, @variables, expand, unwrap, arguments, wrap
 using ModelingToolkit:
     ModelingToolkit,
     ODESystem,
@@ -38,6 +47,7 @@ function ModelingToolkit.ODESystem(eom::HarmonicEquation)
     @assert isone(length(slow_time)) "The argument of the variables are not the same."
     slow_time_ivp = @eval @independent_variables $(Symbol(first(slow_time)))
 
+    # we have the replace the param made with @variables with the ones made with @parameters
     par_names = declare_parameter.(eom.parameters)
 
     eqs = deepcopy(eom.equations)
@@ -45,14 +55,43 @@ function ModelingToolkit.ODESystem(eom::HarmonicEquation)
     eqs = simplify.(expand.(eqs))
     eqs = substitute(eqs, Dict(zip(eom.parameters, par_names)))
 
-    # compute jacobian for performance
     # ∨ mtk v9 need @mtkbuild
     @mtkbuild sys = ODESystem(eqs, first(slow_time_ivp), vars, par_names)
     return sys
 end
 
+function ModelingToolkit.ODESystem(diff_eq::DifferentialEquation)
+    if !is_rearranged_standard(diff_eq)
+        rearrange_standard!(diff_eq)
+    end
+
+    times = get_independent_variables(diff_eq)
+    @assert isone(length(times)) "Only one independent variable allowed."
+    iv = first(@eval @independent_variables $(Symbol(first(times))))
+
+    first_order_transform!(diff_eq, iv)
+
+    eqs = collect(values(diff_eq.equations))
+    vars = get_variables(diff_eq)
+
+    diff_eq_sym = collect(Iterators.flatten(get_variables.(eqs)))
+    param_undeclared = setdiff(setdiff(wrap.(diff_eq_sym), vars), iv)
+    params = declare_parameter.(param_undeclared)
+
+    eqs = substitute(eqs, Dict(zip(param_undeclared, params)))
+
+    @mtkbuild sys = ODESystem(eqs, first(iv), vars, params)
+
+    return sys
+end
+
 function ModelingToolkit.ODEProblem(
-    eom::HarmonicEquation, u0, tspan::Tuple, p::ParameterList; in_place=true, kwargs...
+    eom::Union{HarmonicEquation,DifferentialEquation},
+    u0,
+    tspan::Tuple,
+    p::ParameterList;
+    in_place=true,
+    kwargs...,
 )
     sys = ODESystem(eom)
     param = varmap_to_vars(p, parameters(sys))
@@ -60,7 +99,7 @@ function ModelingToolkit.ODEProblem(
         prob = ODEProblem{false}(sys, u0, tspan, param; jac=true, kwargs...)
     else # in-place
         prob = ODEProblem{true}(sys, u0, tspan, param; jac=true, kwargs...)
-    end
+    end # compute jacobian for performance
     return prob
 end
 
@@ -80,7 +119,7 @@ function ModelingToolkit.SteadyStateProblem(
         prob = SteadyStateProblem{false}(sys, u0, param; jac=true, kwargs...)
     else # in-place
         prob = SteadyStateProblem{true}(sys, u0, param; jac=true, kwargs...)
-    end
+    end # compute jacobian for performance
     return prob
 end
 

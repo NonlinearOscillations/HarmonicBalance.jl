3f44a2d5d25a0611786e0032e66fa0e3d5d4e168
Orjan Ameye <orjan.ameye@hotmail.com>
2024-11-17 08:48:08 +0100

    build: bump version

--- src/HarmonicBalance.jl
+++ src/HarmonicBalance.jl
@@ -91,14 +91,14 @@ using .FFTWExt
 
 using PrecompileTools: @setup_workload, @compile_workload
 
-# @setup_workload begin
-#     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
-#     # precompile file and potentially make loading faster.
-#     @compile_workload begin
-#         # all calls in this block will be precompiled, regardless of whether
-#         # they belong to your package or not (on Julia 1.8 and higher)
-#         include("precompilation.jl")
-#     end
-# end
+@setup_workload begin
+    # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
+    # precompile file and potentially make loading faster.
+    @compile_workload begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to your package or not (on Julia 1.8 and higher)
+        include("precompilation.jl")
+    end
+end
 
 end # module

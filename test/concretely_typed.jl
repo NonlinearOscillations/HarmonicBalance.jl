15d262b0587fba7760b6bcc35270d538849e939a
Orjan Ameye <orjan.ameye@hotmail.com>
2024-11-18 18:25:02 +0100

    concrete type checks?

--- test/concretely_typed.jl
+++ test/concretely_typed.jl
@@ -3,18 +3,16 @@ Pkg.add(; url="https://github.com/gdalle/CheckConcreteStructs.jl")
 
 using CheckConcreteStructs
 
-all_concrete(HarmonicBalance; verbose=true)
 all_concrete(HarmonicBalance.HarmonicVariable)
 all_concrete(HarmonicBalance.WarmUp)
 all_concrete(HarmonicBalance.TotalDegree)
-# all_concrete(HarmonicBalance.Polyhedral) # does not work on CI?
+all_concrete(HarmonicBalance.Polyhedral)
 all_concrete(HarmonicBalance.Result)
 all_concrete(HarmonicBalance.Problem)
 all_concrete(HarmonicBalance.HarmonicEquation)
 all_concrete(HarmonicBalance.DifferentialEquation)
 all_concrete(HarmonicBalance.HarmonicVariable)
 all_concrete(HarmonicBalance.AdiabaticSweep)
-all_concrete(HarmonicBalance.HarmonicVariable)
 
 all_concrete(HarmonicBalance.LinearResponse.Lorentzian)
 all_concrete(HarmonicBalance.LinearResponse.ResponseMatrix)

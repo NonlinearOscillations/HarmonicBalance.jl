6685e8c8a6aaa5bc6e29245f45d2c51bb0153987
Orjan Ameye <orjan.ameye@hotmail.com>
2024-11-18 17:17:40 +0100

    do concrete check seperately

--- test/code_quality.jl
+++ test/code_quality.jl
@@ -3,11 +3,21 @@
     Pkg.add(; url="https://github.com/gdalle/CheckConcreteStructs.jl")
 
     using CheckConcreteStructs
-    all_concrete(HarmonicBalance; verbose=true)
+    all_concrete(HarmonicBalance.HarmonicVariable)
+    all_concrete(HarmonicBalance.WarmUp)
+    all_concrete(HarmonicBalance.TotalDegree)
+    all_concrete(HarmonicBalance.Polyhedral)
+    all_concrete(HarmonicBalance.Result)
+    all_concrete(HarmonicBalance.Problem)
+    all_concrete(HarmonicBalance.HarmonicEquation)
+    all_concrete(HarmonicBalance.DifferentialEquation)
+    all_concrete(HarmonicBalance.HarmonicVariable)
+    all_concrete(HarmonicBalance.AdiabaticSweep)
+    all_concrete(HarmonicBalance.HarmonicVariable)
+
     all_concrete(HarmonicBalance.LinearResponse.Lorentzian)
     all_concrete(HarmonicBalance.LinearResponse.ResponseMatrix)
     all_concrete(HarmonicBalance.LinearResponse.JacobianSpectrum)
-end
 
 @testset "Code linting" begin
     using JET

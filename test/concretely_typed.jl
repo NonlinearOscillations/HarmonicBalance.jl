b5d9d756b11bfd23d31ea143725a097e8cffa840
Orjan Ameye <orjan.ameye@hotmail.com>
2024-11-18 19:32:54 +0100

    don't run conrete tests on CI

--- test/concretely_typed.jl
+++ test/concretely_typed.jl
@@ -1,19 +1,22 @@
-using Pkg, HarmonicBalance
-Pkg.add(; url="https://github.com/gdalle/CheckConcreteStructs.jl")
+CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+if !CI
+    using Pkg, HarmonicBalance
+    Pkg.add(; url="https://github.com/gdalle/CheckConcreteStructs.jl")
 
-using CheckConcreteStructs
+    using CheckConcreteStructs
 
-all_concrete(HarmonicBalance.HarmonicVariable)
-all_concrete(HarmonicBalance.WarmUp)
-all_concrete(HarmonicBalance.TotalDegree)
-all_concrete(HarmonicBalance.Polyhedral)
-all_concrete(HarmonicBalance.Result)
-all_concrete(HarmonicBalance.Problem)
-all_concrete(HarmonicBalance.HarmonicEquation)
-all_concrete(HarmonicBalance.DifferentialEquation)
-all_concrete(HarmonicBalance.HarmonicVariable)
-all_concrete(HarmonicBalance.AdiabaticSweep)
+    all_concrete(HarmonicBalance.HarmonicVariable)
+    all_concrete(HarmonicBalance.WarmUp)
+    all_concrete(HarmonicBalance.TotalDegree)
+    all_concrete(HarmonicBalance.Polyhedral)
+    all_concrete(HarmonicBalance.Result)
+    all_concrete(HarmonicBalance.Problem)
+    all_concrete(HarmonicBalance.HarmonicEquation)
+    all_concrete(HarmonicBalance.DifferentialEquation)
+    all_concrete(HarmonicBalance.HarmonicVariable)
+    all_concrete(HarmonicBalance.AdiabaticSweep)
 
-all_concrete(HarmonicBalance.LinearResponse.Lorentzian)
-all_concrete(HarmonicBalance.LinearResponse.ResponseMatrix)
-all_concrete(HarmonicBalance.LinearResponse.JacobianSpectrum)
+    all_concrete(HarmonicBalance.LinearResponse.Lorentzian)
+    all_concrete(HarmonicBalance.LinearResponse.ResponseMatrix)
+    all_concrete(HarmonicBalance.LinearResponse.JacobianSpectrum)
+end

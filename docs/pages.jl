c3a31948eba59a01f0fc523d9650b9c87df23ef8
Orjan Ameye <orjan.ameye@hotmail.com>
2024-11-21 12:48:49 +0100

    add API list

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,35 +1,36 @@
 #! format: off
 pages = [
     "Home" => "index.md",
-    "Getting Started" => [
-        "Brief Example" => "introduction/index.md",
-        "Citations" => "introduction/citation.md",
-    ],
+    "Getting Started" => "introduction/index.md",
     "Theoretical Background" => [
-        "Floquet expansions" => "background/harmonic_balance.md"
-        "Stability and Linear Response" => "background/stability_response.md"
-        "Limit Cycles" => "background/limit_cycles.md"
+        "Floquet expansions" => "background/harmonic_balance.md",
+        "Stability and Linear Response" => "background/stability_response.md",
+        "Limit Cycles" => "background/limit_cycles.md",
     ],
     "Tutorials" => [
-        "Steady States" => "tutorials/steady_states.md"
-        "Classifying Solutions" => "tutorials/classification.md"
-        "Linear Response" => "tutorials/linear_response.md"
-        "Transient Dynamics" => "tutorials/time_dependent.md"
-        "Limit Cycles" => "tutorials/limit_cycles.md"
-    ],
-    "Examples" => [
+        "Steady States" => "tutorials/steady_states.md",
+        "Classifying Solutions" => "tutorials/classification.md",
+        "Linear Response" => "tutorials/linear_response.md",
+        "Transient Dynamics" => "tutorials/time_dependent.md",
+        "Limit Cycles" => "tutorials/limit_cycles.md",
+        "Examples" => [
         "Wave Mixing" => "examples/wave_mixing.md"
         "Parametric Three Wave Mixing" => "examples/parametric_via_three_wave_mixing.md"
         "Parametric Oscillator" => "examples/parametron.md"
+        ],
     ],
-    "Manual" => [
-        "Entering Equations of Motion" => "manual/entering_eom.md"
-        "Computing Effective System" => "manual/extracting_harmonics.md"
-        "Computing Steady States" => "manual/methods.md"
-        "Krylov-Bogoliubov" => "manual/Krylov-Bogoliubov_method.md"
-        "Time Evolution" => "manual/time_dependent.md"
-        "Linear Response" => "manual/linear_response.md"
-        "Plotting" => "manual/plotting.md"
-        "Saving and Loading" => "manual/saving.md"
-    ]
+    "Resources" => [
+        "API" => "manual/API.md",
+        "Bibliography" => "introduction/citation.md",
+        "Manual" => [
+        "Entering Equations of Motion" => "manual/entering_eom.md",
+        "Computing Effective System" => "manual/extracting_harmonics.md",
+        "Computing Steady States" => "manual/methods.md",
+        "Krylov-Bogoliubov" => "manual/Krylov-Bogoliubov_method.md",
+        "Time Evolution" => "manual/time_dependent.md",
+        "Linear Response" => "manual/linear_response.md",
+        "Plotting" => "manual/plotting.md",
+        "Saving and Loading" => "manual/saving.md",
+        ]
+    ],
 ]
---
+++ docs/src/manual/API.md
@@ -0,0 +1,103 @@
+```@meta
+CurrentModule = HarmonicBalance
+```
+
+# [API](@id doc-API)
+
+**Table of contents**
+
+[[toc]] <!-- the level setting is in ".vitepress/config.mts" -->
+
+## System objects and types
+
+```@docs
+DifferentialEquation
+HarmonicVariable
+HarmonicEquation
+```
+```@docs
+rearrange_standard
+rearrange_standard!
+first_order_transform!
+is_rearranged_standard
+get_equations
+```
+```@docs
+get_harmonic_equations
+get_krylov_equations
+add_harmonic!
+```
+```@docs
+get_independent_variables
+get_variables
+```
+
+## Solving and transforming solutions
+
+```@docs
+get_steady_states
+```
+
+### Methods
+```@docs
+WarmUp
+TotalDegree
+Polyhedral
+```
+
+### Access solutions
+```@docs
+get_single_solution
+transform_solutions
+```
+
+### Classify
+```@docs
+classify_solutions!
+get_class
+```
+
+### Plotting
+
+```@docs
+plot
+plot_phase_diagram
+plot_spaghetti
+```
+
+## Limit cycles
+```@docs
+get_limit_cycles
+get_cycle_variables
+add_pairs!
+```
+
+## Linear Response
+
+```@docs
+get_Jacobian
+```
+
+```@docs
+plot_eigenvalues
+plot_linear_response
+plot_rotframe_jacobian_response
+```
+
+## Extentsions
+
+### OrdinaryDiffEq
+```@docs
+AdiabaticSweep
+follow_branch
+plot_1D_solutions_branch
+```
+
+### SteadyStateSweep
+```@docs
+steady_state_sweep
+```
+
+### ModelingToolkit
+```@docs
+```

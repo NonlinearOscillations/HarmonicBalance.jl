26ee8fca0d559a2c6a7823aee79ca24d1f49ae02
Ssalazar12 <slzr000@gmail.com>
2024-11-26 17:27:39 +0100

    created function to save the data in an efficient manner in src/save_results.jl. We still need to save the number of solutions in the jl2 file

---
+++ src/save_results.jl
@@ -0,0 +1,37 @@
+using FileIO
+
+
+function export_results_dict(res::Result)
+	# takes an instance of the Result Structure and ommits the NaN's
+	# by creating a dictionary at each elelement of Result.solutions
+	# which only saves the non-Nan indices and values
+
+    dimensions_parameters = size(res.solutions)
+    dimension_solutions = length(res.solutions[1])
+        
+    # THis is a janky way of asking whether we sweep through 1 or 2 parameters
+    if length(dimensions_parameters) == 1
+        M=Array{Dict}(undef, dimensions_parameters[1])
+    elseif length(dimensions_parameters) == 2
+        M=Array{Dict}(undef, dimensions_parameters[1],dimensions_parameters[2])
+    end
+
+    # Iterate over each of the array elements
+    for iter in eachindex(res.solutions)
+        A = res.solutions[iter]
+        value_list= []
+        key_list = []
+        # ask wether the solution is NaN
+        for i in 1:dimension_solutions
+            if isnan(A[i][1]) == false
+                push!(key_list, i)
+                push!(value_list, A[i])
+            end
+        end
+        # save a dictionary at each element of the M matrix
+        M[iter] = Dict(key_list .=> value_list)
+    end
+    return M  
+end
+
+

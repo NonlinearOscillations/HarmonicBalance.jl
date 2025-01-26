da8077ce66ddd93add4d2faaacdc600425a742f6
Orjan Ameye <orjan.ameye@hotmail.com>
2025-01-26 20:30:00 +0100

    fix implicit jacobian test

--- test/steady_states/parametron.jl
+++ test/steady_states/parametron.jl
@@ -61,7 +61,7 @@ method = HarmonicBalance.WarmUp(; seed=SEED)
     @testset "implicit jacobian" begin
         harmonic_eq = get_harmonic_equations(dEOM; jacobian=false)
         p = HarmonicBalance.HomotopyContinuationProblem(harmonic_eq, varied, fixed)
-        @test round.(real.(p.jacobian([0, 0]))) == [-98.0 0.0; 0.0 -102.0]
+        @test round.(real.(p.jacobian(zeros(3)))) == [-98.0 0.0; 0.0 -102.0]
         res = get_steady_states(p, method; show_progress=false)
     end
 

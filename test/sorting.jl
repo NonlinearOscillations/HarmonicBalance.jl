f019c185b50ca8cb8c3c14e0c2438f6c4e8e0344
Orjan Ameye <orjan.ameye@hotmail.com>
2025-02-16 22:40:39 +0100

    format

--- test/sorting.jl
+++ test/sorting.jl
@@ -11,7 +11,11 @@ add_harmonic!(diff_eq, x, ω);
 harmonic_eq = get_harmonic_equations(diff_eq)
 
 fixed = (ω₀ => 1.0, γ => 0.002, α => 1.0)
-varied = (ω => range(0.99, 1.01, 100),λ => range(1e-6, 0.03, 100))
+varied = (ω => range(0.99, 1.01, 100), λ => range(1e-6, 0.03, 100))
 
 result_2D = get_steady_states(harmonic_eq, varied, fixed; verbose=true)
-result_2D_hilbert = get_steady_states(harmonic_eq, varied, fixed; sorting="hilbert", verbose=true)
+result_2D_hilbert = get_steady_states(
+    harmonic_eq, varied, fixed; sorting="hilbert", verbose=true
+)
+
+# ∨ compare sorting solutions

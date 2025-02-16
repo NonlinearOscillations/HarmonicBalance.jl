6a7d2e28e62ff3d185383fd8b2811bb53ca4870c
Orjan Ameye <orjan.ameye@hotmail.com>
2025-02-16 15:53:02 +0100

    format

--- examples/MTKHomotopy.jl
+++ examples/MTKHomotopy.jl
@@ -1,5 +1,5 @@
 using HarmonicBalance, ModelingToolkit, NonlinearSolve
-using HarmonicBalance:  OrderedDict
+using HarmonicBalance: OrderedDict
 using NonlinearSolveHomotopyContinuation
 
 begin
@@ -12,7 +12,7 @@ begin
     fn = NonlinearFunction(rhs; jac)
 
     prob = NonlinearProblem(fn, 1.0, [5.0, 6.0])
-    _alg = HomotopyContinuationJL{true}(; threading = false)
+    _alg = HomotopyContinuationJL{true}(; threading=false)
     sol = solve(prob, _alg)
 end
 
@@ -28,27 +28,30 @@ natural_equation =
 forces = F * cos(ω * t)
 diff_eq = DifferentialEquation(natural_equation + forces, x)
 
-
 add_harmonic!(diff_eq, x, ω);
 harmonic_eq = get_harmonic_equations(diff_eq)
 eqs = getfield.(rearrange_standard(harmonic_eq).equations, :lhs) .~ 0
 
 begin
-fixed = OrderedDict(ω₀ => 1.0, γ => 1e-2, λ => 5e-2, F => 1e-3, α => 1.0, η => 0.3, ω => 1.0)
-@mtkbuild  ns = NonlinearSystem(eqs, get_variables(harmonic_eq),keys(fixed))
+    fixed = OrderedDict(
+        ω₀ => 1.0, γ => 1e-2, λ => 5e-2, F => 1e-3, α => 1.0, η => 0.3, ω => 1.0
+    )
+    @mtkbuild ns = NonlinearSystem(eqs, get_variables(harmonic_eq), keys(fixed))
 
-fixed = OrderedDict(ω₀ => 1.0, γ => 1e-2, λ => 5e-2, F => 1e-3, α => 1.0, η => 0.3, ω => 1.0)
-fn = NonlinearFunction(ns, jac=true)
-hcp = NonlinearProblem(fn, [0.0,0.0], fixed)
-solve(hcp)
-alg = HomotopyContinuationJL{true}(;)
-solve(hcp, alg)
+    fixed = OrderedDict(
+        ω₀ => 1.0, γ => 1e-2, λ => 5e-2, F => 1e-3, α => 1.0, η => 0.3, ω => 1.0
+    )
+    fn = NonlinearFunction(ns; jac=true)
+    hcp = NonlinearProblem(fn, [0.0, 0.0], fixed)
+    solve(hcp)
+    alg = HomotopyContinuationJL{true}(;)
+    solve(hcp, alg)
 end
 begin
-    @mtkbuild  ns = NonlinearSystem(eqs, get_variables(harmonic_eq),harmonic_eq.parameters)
+    @mtkbuild ns = NonlinearSystem(eqs, get_variables(harmonic_eq), harmonic_eq.parameters)
 
     fixed = (ω₀ => 1.0, γ => 1e-2, λ => 5e-2, F => 1e-3, α => 1.0, η => 0.3, ω => 1.0)
-    hcp = HomotopyContinuationProblem(ns, [0.0,0.0], fixed)
+    hcp = HomotopyContinuationProblem(ns, [0.0, 0.0], fixed)
     solve(hcp)
     alg = HomotopyContinuationJL{true}(;)
     solve(hcp, alg)

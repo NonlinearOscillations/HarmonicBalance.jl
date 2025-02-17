cccb30e5e5c19b17b1a48902ef7a3ebcf93f0707
Orjan Ameye <orjan.ameye@hotmail.com>
2025-02-17 08:09:42 +0100

    fix: isolate sorting test (#388)
    * feat: add verbose keyword the homotopy solver
    
    * test: test hilbert sorting
    
    * format
    
    * fix: isolate sorting test
    
    * format

--- test/sorting.jl
+++ test/sorting.jl
@@ -1,5 +1,5 @@
+
 using HarmonicBalance
-using HomotopyContinuation
 
 @variables ω₀ γ λ α ω t x(t)
 
@@ -13,9 +13,11 @@ harmonic_eq = get_harmonic_equations(diff_eq)
 fixed = (ω₀ => 1.0, γ => 0.002, α => 1.0)
 varied = (ω => range(0.99, 1.01, 100), λ => range(1e-6, 0.03, 100))
 
-result_2D = get_steady_states(harmonic_eq, varied, fixed; verbose=true)
-result_2D_hilbert = get_steady_states(
-    harmonic_eq, varied, fixed; sorting="hilbert", verbose=true
-)
+@testset "hilbert" begin
+    result_2D = get_steady_states(harmonic_eq, varied, fixed; verbose=true)
+    result_2D_hilbert = get_steady_states(
+        harmonic_eq, varied, fixed; sorting="hilbert", verbose=true
+    )
+end
 
 # ∨ compare sorting solutions

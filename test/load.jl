2615dd929ec5b4092a5b806e7a375c317f3957aa
Orjan Ameye <orjan.ameye@hotmail.com>
2025-03-01 17:37:13 +0100

    fix: remove saving util (#399)

--- test/load.jl
+++
@@ -1,5 +0,0 @@
-import HarmonicBalance.load
-
-# load the previously-saved result
-current_path = @__DIR__
-@test load(current_path * "/parametron_result.jld2") isa HarmonicBalance.Result skip = true
